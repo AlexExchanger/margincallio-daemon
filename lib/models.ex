5368dd29975ea86f2d8c5aba5229ae5255924116
INSIDER <unexisted_email@example.com>
2015-01-08 17:49:20 +0300

    fix EXDAEMON-2

--- lib/models.ex
+++ lib/models.ex
@@ -2,18 +2,36 @@ defmodule Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
   def conf do
-    parse_url "ecto://luciding:easypass@localhost/luciding"
+    parse_url "ecto://postgres:password@localhost/exchange"
   end
 end
 
-defmodule User do
+defmodule Deal do
     use Ecto.Model
 
-    schema "lu_user" do
-        field :username,    :string
-        field :email, :string
-        field :password, :string
-        field :status,    :string
-        field :today_dream_id, :integer
+    schema "deal_BTCUSD" do
+        field :size,    :decimal
+        field :price, :decimal
+        field :orderBuyId, :integer
+        field :orderSellId, :integer
+        field :createdAt, :integer
+        field :userBuyId, :integer
+        field :userSellId, :integer
+    end
+end
+
+defmodule Order do
+    use Ecto.Model
+
+    schema "order_BTCUSD" do
+        field :userId, :integer
+        field :size,    :decimal
+        field :rate, :decimal
+        field :price, :decimal
+        field :createdAt, :integer
+        field :updatedAt, :integer
+        field :status, :string
+        field :type, :string
+        field :side, :boolean
     end
 end
